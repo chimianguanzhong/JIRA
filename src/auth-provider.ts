b4b765c3091f94a547deaf74a56ff0e21f892048
charlie <ljwcharlie@gmail.com>
2022-12-16 16:16:44 +0800

    fix: xxx

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -1,19 +1,22 @@
+// 这个文件会定义一些函数，用来操控JWT的Token
 // 在真实环境中，如果使用firebase这种第三方auth服务的话，本文件不需要开发者开发
 
 import { User } from "types/user";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
-const localStorageKey = "__auth_provider_token__";
+const localStorageKey = "__auth_provider_token__"; // 保存用户输入时的key
 
-export const getToken = () => window.localStorage.getItem(localStorageKey);
+export const getToken = () => window.localStorage.getItem(localStorageKey); // 获取Token
 
 export const handleUserResponse = ({ user }: { user: User }) => {
+  // 处理服务器响应的用户数据
   window.localStorage.setItem(localStorageKey, user.token || "");
   return user;
 };
 
 export const login = (data: { username: string; password: string }) => {
+  // 处理用户登录
   return fetch(`${apiUrl}/login`, {
     method: "POST",
     headers: {
@@ -30,6 +33,7 @@ export const login = (data: { username: string; password: string }) => {
 };
 
 export const register = (data: { username: string; password: string }) => {
+  // 处理用户注册
   return fetch(`${apiUrl}/register`, {
     method: "POST",
     headers: {
@@ -46,4 +50,5 @@ export const register = (data: { username: string; password: string }) => {
 };
 
 export const logout = async () =>
+  // 处理退出登录
   window.localStorage.removeItem(localStorageKey);
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -15,20 +15,25 @@ import { useAsync } from "utils/use-async";
 // const test = (p: Base) => {
 // }
 //
-// // 鸭子类型(duck typing)：面向接口编程 而不是 面向对象编程
-// const a = {id: 1, name: 'jack'}
-// test(a)
-// const apiUrl = process.env.REACT_APP_API_URL;
+// 鸭子类型(duck typing)：面向接口编程 而不是 面向对象编程
+// const a = {id: 1, name: 'jack'}  // Base里面有的属性a都有
+// test(a)  // 所以虽然test接收的是Base类型的参数，但是a比Base高级，所以a也可以被接收，这就是ts中的类型兼容
+// const apiUrl = process.env.REACT_A PP_API_URL;
 
 export const LoginScreen = ({
   onError,
 }: {
   onError: (error: Error) => void;
 }) => {
-  const { login } = useAuth();
+  const { login } = useAuth(); // 将发送post请求的login方法封装到useAuth hook 中
   const { run, isLoading } = useAsync(undefined, { throwOnError: true });
 
   // HTMLFormElement extends Element
+  /**
+   * ts中的类型兼容：
+   * Element 有的类型 HTMLFormElement 一定有
+   * HTMLFormElement 有的类型 Element 不一定有
+   */
   const handleSubmit = async (values: {
     username: string;
     password: string;
@@ -40,7 +45,9 @@ export const LoginScreen = ({
     }
   };
 
+  // 编写表单
   return (
+    // 获取表单里的数据 handleSubmit。表单提交的时候， form会给handleSubmit传递event，event就是HTMLFormElement类型
     <Form onFinish={handleSubmit}>
       <Form.Item
         name={"username"}
